22204bba4849fb8edd6072adf2e2e6698b4b0ebf
BuildTools <unconfigured@null.spigotmc.org>
2023-05-16 23:16:48 +0300

    Clean up code and use faster solution to get the order

--- app/src/main/java/com/ihu/e_shopmanager/orders/CompletionOfOrder.java
+++ app/src/main/java/com/ihu/e_shopmanager/orders/CompletionOfOrder.java
@@ -1,18 +1,12 @@
 package com.ihu.e_shopmanager.orders;
 
-import android.Manifest;
 import android.annotation.SuppressLint;
-import android.app.NotificationChannel;
-import android.app.NotificationManager;
 import android.content.Context;
-import android.content.pm.PackageManager;
 import android.content.res.Configuration;
-import android.os.Build;
 import android.os.Bundle;
 import android.os.Vibrator;
 import android.text.Editable;
 import android.text.TextWatcher;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -22,34 +16,23 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import androidx.core.app.ActivityCompat;
-import androidx.core.app.NotificationCompat;
-import androidx.core.app.NotificationManagerCompat;
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
-import com.google.firebase.firestore.CollectionReference;
-import com.google.firebase.firestore.DocumentSnapshot;
 import com.ihu.e_shopmanager.MainActivity;
 import com.ihu.e_shopmanager.R;
 import com.ihu.e_shopmanager.clients.Client;
-import com.ihu.e_shopmanager.products.Product;
-import com.ihu.e_shopmanager.products.ProductWithQuantity;
 import com.ihu.e_shopmanager.sales.Sale;
 
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
 import java.util.Date;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 public class CompletionOfOrder extends Fragment {
 
-    private HashMap<Integer, Order> orderMap = new HashMap<>();
 
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
         View view;
         int orientation = getResources().getConfiguration().orientation;
@@ -61,13 +44,6 @@ public class CompletionOfOrder extends Fragment {
 
         TextView toolbarText = requireActivity().findViewById(R.id.toolbar_string);
         toolbarText.setText("Παραγγελίες");
-
-        List<Order> orders = MainActivity.myAppDatabase.myDao().getOrders();
-
-
-        for (Order order : orders) {
-            orderMap.put(order.getId(), order);
-        }
 
         registerListeners(view);
 
@@ -109,7 +85,7 @@ public class CompletionOfOrder extends Fragment {
                     order_total_price_view.setText(" Σύνολο: 0€");
                 } else {
                     int orderID = parseInt(s.toString());
-                    Order order = orderMap.get(orderID);
+                    Order order = MainActivity.myAppDatabase.myDao().getOrderFromId(orderID);
 
                     if (order == null) {
                         order_client_view.setText(" Πελάτης: ");
@@ -131,7 +107,7 @@ public class CompletionOfOrder extends Fragment {
                     order_client_view.setText(" Πελάτης: " + client.getName() + " " + client.getLastname());
                     dateView.setText(" Ημερομηνία Παραγγελίας: " + order.getOrderDate());
                     order_id_view.setText(" Παραγγελία: " + orderID);
-                    String formattedPrice = String.format("%.2f", order.getTotalPrice());
+                    @SuppressLint("DefaultLocale") String formattedPrice = String.format("%.2f", order.getTotalPrice());
                     order_total_price_view.setText(" Σύνολο: " + formattedPrice + "€");
 
                 }
@@ -144,12 +120,12 @@ public class CompletionOfOrder extends Fragment {
             vibrator.vibrate(40);
 
             int id = parseInt(order_finish_order_id.getText().toString());
-            Order order = orderMap.get(id);
+            Order order = MainActivity.myAppDatabase.myDao().getOrderFromId(id);
             if (order != null) {
                 try {
                     Sale sale = new Sale();
                     Date currentDate = new Date();
-                    SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+                    @SuppressLint("SimpleDateFormat") SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
                     String formattedDate = dateFormat.format(currentDate);
                     sale.setSale_date(formattedDate);
                     sale.setProductsList(order.getProducts());
@@ -157,26 +133,23 @@ public class CompletionOfOrder extends Fragment {
                     sale.setClient_id(order.getClientId());
                     sale.setValue(order.getTotalPrice());
                     sale.setSale_id(order.getId());
-                    CollectionReference salesReference = MainActivity.firestoreDatabase.collection("Sales");
-                    List<ProductWithQuantity> products = new ArrayList<>();
-                    List<Sale> sales = new ArrayList<>();
 
                     MainActivity.firestoreDatabase.collection("Sales").
                             document("" + order.getId()).
                             set(sale).addOnCompleteListener(task -> {
-                                Toast.makeText(getActivity(), "Η παραγγελία ολοκληρώθηκε επιτυχώς", Toast.LENGTH_LONG).show();
+                                Toast.makeText(getActivity(), "Η παραγγελία ολοκληρώθηκε επιτυχώς.", Toast.LENGTH_LONG).show();
                                 MainActivity.myAppDatabase.myDao().deleteOrder(order);
                             })
-                            .addOnFailureListener(e -> Toast.makeText(getActivity(), "Η παραγγελία δεν ολοκληρώθηκε", Toast.LENGTH_LONG).show());
+                            .addOnFailureListener(e -> Toast.makeText(getActivity(), "Η παραγγελία δεν ολοκληρώθηκε.", Toast.LENGTH_LONG).show());
                 } catch (Exception e) {
                     String message = e.getMessage();
                     System.out.println("ERROR: " + message);
                     Toast.makeText(getActivity(), message, Toast.LENGTH_LONG).show();
                 }
             } else
-                Toast.makeText(getActivity(), "Δε βρέθηκε παραγγελία ", Toast.LENGTH_LONG).show();
+                Toast.makeText(getActivity(), "Δε βρέθηκε παραγγελία.", Toast.LENGTH_LONG).show();
 
-            InputMethodManager imm = (InputMethodManager) getActivity().getSystemService(Context.INPUT_METHOD_SERVICE);
+            InputMethodManager imm = (InputMethodManager) requireActivity().getSystemService(Context.INPUT_METHOD_SERVICE);
             imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
 
             order_client_view.setText(" Πελάτης: ");
